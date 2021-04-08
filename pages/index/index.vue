d9fa886ef99a092dc38e30b04bed113b985ee472
hans <im.hansking@gmail.com>
2021-04-08 20:35:00 +0800

    fix: 初始化云函数，初始化自动化爬虫

---
+++ pages/index/index.vue
@@ -0,0 +1,78 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			this.get()
+		},
+		methods: {
+			get() {
+				uni.showLoading({
+					title: '处理中...'
+				})
+				uniCloud.callFunction({
+					name: 'get-weibo-hot',
+					data: {
+						name: 'DCloud',
+						subType: 'uniCloud',
+						createTime: Date.now()
+					}
+				}).then((res) => {
+					uni.hideLoading()
+					uni.showModal({
+						content: `查询成功，获取数据列表为：${JSON.stringify(res.result.data)}`,
+						showCancel: false
+					})
+					console.log(res)
+				}).catch((err) => {
+					uni.hideLoading()
+					uni.showModal({
+						content: `查询失败，错误信息为：${err.message}`,
+						showCancel: false
+					})
+					console.error(err)
+				})
+			},
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
---
+++ uniCloud-aliyun/cloudfunctions/get-weibo-hot/index.js
@@ -0,0 +1,14 @@
+'use strict';
+const db = uniCloud.database()
+/**
+ * @event {pageNum: Number, pageSize: Number}
+ * 
+ */
+exports.main = async (event, context) => {
+	event.pageNum = 0
+	event.pageSize = 5
+	console.log(event)
+	const collection = db.collection('weibo-hot')
+	const res = await collection.skip(event.pageNum).limit(event.pageSize).orderBy("create_time", "desc").get()
+	return res
+};
