a8dfc16b3e2f34ff528afde61965913a2814a94e
hans <im.hansking@gmail.com>
2021-04-12 20:14:14 +0800

    fix (v1.0.1) [upgrade-center]: add App 内部云端更新

--- App.vue
+++ App.vue
@@ -1,13 +1,14 @@
-
 <style lang="scss">
 	/* 注意要写在第一行，同时给style标签加入lang="scss"属性 */
 	@import "uview-ui/index.scss";
 </style>
 
 <script>
+	import checkUpdate from '@/uni_modules/uni-upgrade-center-app/utils/check-update';
 	export default {
 		onLaunch: function() {
 			console.log('App Launch')
+			checkUpdate() //更新升级
 		},
 		onShow: function() {
 			console.log('App Show')
@@ -19,5 +20,5 @@
 </script>
 
 <style>
-	
+
 </style>
