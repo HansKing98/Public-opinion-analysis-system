f93c05346113d5656cb04e8199a63d15995d2035
hans <>
2021-05-26 23:19:22 +0800

    fix: 回滚 删除侧滑页

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,40 +1,118 @@
 <template>
-	<div>
-		<vue-drawer-layout ref="drawer" :drawer-width="300" :enable="true" :animatable="true" :z-index="0"
-			:drawable-distance="Math.floor(800/3)" :content-drawable="true" :backdrop="true"
-			:backdrop-opacity-range="[0,0.4]">
-
-			<div class="drawer-content" slot="drawer">
-				<!-- drawer-content -->
-				<my />
-			</div>
-			<div slot="content">
-				<!-- main-content -->
-				<div>
-					<hot ref="hot"></hot>
-				</div>
-			</div>
-		</vue-drawer-layout>
-	</div>
+	<view class="content">
+		<view class="card-bg">
+			<leo-list :show-icon="true" :show-arrow="true" :list="list"></leo-list>
+			<skeleton :loading="loading" :avatarSize="skeleton1.avatarSize" :avatarShape="'square'" :row="skeleton1.row"
+				:showTitle="skeleton1.showTitle" :animate="skeleton1.animate" v-for="(i,index) in 8" :key="index">
+			</skeleton>
+			<u-loadmore :status="status" @loadmore="getMore()" />
+		</view>
+	</view>
 </template>
 
 <script>
-	import hot from './hot.vue'
-	import my from '../my/my.vue'
 	export default {
-		components: {
-			hot,
-			my
+		data() {
+			return {
+				title: 'Hello',
+				list: [],
+				status: 'loadmore',
+				pageData: {
+					page: 0,
+					pageSize: 11,
+				},
+				loading: true,
+				skeleton1: {
+					avatarSize: '40px',
+					row: 1,
+					showTitle: true,
+					animate: true
+				}
+			}
 		},
-		onReachBottom() {
-			console.log(12)
+		mounted() {
+			this.get()
 		},
 		onPullDownRefresh() {
-			this.$refs['hot'].refresh()
+			this.pageData.page = 0
+			// console.log(this.pageData.page)
+			this.get();
 		},
+		methods: {
+			get() {
+				uni.showLoading({
+					title: '正在加载更多...'
+				})
+
+				uniCloud.callFunction({
+					name: 'get-weibo-hot',
+					data: this.pageData
+				}).then((res) => {
+					if (this.pageData.page == 0) {
+						this.list = []
+					}
+					this.list.push.apply(this.list, res.result.data);
+					// 是否读到最后一条
+					// console.log(this.list.length)
+					if (res.result.data.length == 0) {
+						// 已经加载了全部数据
+						this.status = "nomore"
+					}
+					this.loading = false;
+					uni.hideLoading()
+					// uni.showModal({
+					// 	content: `查询成功，获取数据列表为：${JSON.stringify(res.result.data)}`,
+					// 	showCancel: false
+					// })
+					// console.log(res)
+				}).catch((err) => {
+					uni.hideLoading()
+					uni.showModal({
+						content: `查询失败，错误信息为：${err.message}`,
+						showCancel: false
+					})
+					console.error(err)
+				}).finally(() => {
+					uni.stopPullDownRefresh()
+				})
+			},
+			getMore() {
+				this.status = 'loading';
+				this.pageData.page += 1
+				this.get()
+			}
+		},
+		// 触底事件
+		onReachBottom() {
+			this.getMore()
+		}
 	}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+	page {
+		background: #f5f7fa;
+	}
 
+	.content {
+		/* #ifdef APP-PLUS */
+		padding-top: var(--status-bar-height);
+		/* #endif */
+		/* #ifndef APP-PLUS */
+		padding-top: 1rpx;
+		/* #endif */
+		padding-bottom: 1rpx;
+		// min-height: calc(100vh - var(--status-bar-height));
+		// background: linear-gradient(to, #3377ff,  #f5f7fa 564rpx);
+
+		background: #f5f7fa;
+	}
+
+	.card-bg {
+		margin: 24rpx 24rpx;
+		padding: 24rpx 32rpx;
+		background: #ffffff;
+		border-radius: 20rpx;
+		box-shadow: 0px 4rpx 20rpx 0rpx rgba(235, 241, 244, 0.35);
+	}
 </style>
